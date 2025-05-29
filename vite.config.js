d7ce89898a98a37b6d8b29f8d3b59f23773b6422
giftty <wwwgift@yahoo.com>
2025-05-29 05:36:03 +0100

    depoment

--- vite.config.js
+++ vite.config.js
@@ -47,4 +47,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/tag_generator/'  // Replace with your GitHub repo name
+    : '/'
 })
